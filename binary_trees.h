5925ca0674dddd776a79c170fb61b16f4d34c823
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-27 12:52:19 +0000

    Task 00

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef _BINARY_TREES_H
 #define _BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -19,5 +22,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
