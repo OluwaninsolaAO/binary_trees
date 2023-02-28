08ac2db2d7393a7d00ad42697eddde9e1f8bcad7
FearGod12 <onyenikechukwudi@gmail.com>
2023-02-28 18:25:26 +0000

    fixed betty errors

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	if (grandparent->left == node->parent)
-		return grandparent->right;
+		return (grandparent->right);
 	else
-		return grandparent->left;
+		return (grandparent->left);
 }
