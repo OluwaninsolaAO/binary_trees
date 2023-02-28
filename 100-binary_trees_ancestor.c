412e4839113c2fa76454653993b6742678248b1c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-01 00:36:33 +0100

    Task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest commen ancestor of
+ * two nodes.
+ * @first: being a pointer to the first node.
+ * @second: being a pointer to the second node.
+ * Return: pointer to the lowerst common ancestor node of the
+ * two given nodes or NULL if no common ancestor was found.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *node_a, *node_b;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	node_a = (binary_tree_t *) first;
+	node_b = (binary_tree_t *) second;
+
+	while (node_a != NULL)
+	{
+		if (node_a == node_b->parent)
+			return (node_a);
+
+		node_b = node_b->parent;
+
+		if (node_b->parent == NULL)
+		{
+			node_b = (binary_tree_t *) second;
+			node_a = node_a->parent;
+		}
+	}
+	return (NULL);
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 	if (node->parent->parent == NULL)
 		return (NULL);
-	if(node->parent->parent->left == node->parent)
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
 }
