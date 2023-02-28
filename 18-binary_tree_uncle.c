1aeaa0012fd6e0cd8ad6d5742f541a04a9f62b95
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-01 00:03:08 +0100

    Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,12 +8,13 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent = node->parent->parent;
-
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL)
 		return (NULL);
-	if (grandparent->left == node->parent)
-		return (grandparent->right);
-	else
-		return (grandparent->left);
+	if (node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+		return (NULL);
+	if(node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
