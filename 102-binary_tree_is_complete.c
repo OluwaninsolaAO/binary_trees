e5737733acc615d63b6f7112b881d48003e9f8a0
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-02 01:06:45 +0100

    Task 102: Null

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete.
+ * @tree: being a pointer to the root node of the tree to check.
+ * Return: 1 if tree is complete or 0 if otherwise or tree is NULL.
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1);
+}
